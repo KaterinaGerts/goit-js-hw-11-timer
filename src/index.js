5908fa2eda10047870850e345befe3bf79bc30c6
KaterinaGerts <75572090+KaterinaGerts@users.noreply.github.com>
2021-06-15 13:22:01 +0300

    начала делать таймер

---
+++ src/index.js
@@ -0,0 +1,62 @@
+const refs = {
+timerEl: document.querySelector('#timer-1'),
+daysEl: document.querySelector('[data-value="days"]'),
+hoursEl: document.querySelector('[data-value="hours"]'),
+minsEl: document.querySelector('[ data-value="mins"]'),
+secEl: document.querySelector('[data-value="secs"]')
+};
+console.log(refs.timerEl);
+
+ class CountdownTimer {
+  constructor ({onTick}) {
+    this.intervalId = null;
+    this.onTick = onTick;
+
+    this.init();
+  }
+  init() {
+    const time = this.getTimeComponents(0);
+    this.onTick(time);
+  }
+
+start() {
+    const startTime = Date.now();    
+    this.intervalId = setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = currentTime - startTime;
+      const time = this.getTimeComponents(deltaTime);
+
+      this.onTick(time);
+    }, 1000); 
+  }
+  // start();
+
+  stop() {
+    clearInterval(this.intervalId);
+    const time = this.getTimeComponents(0);
+    this.onTick(time);
+  }
+  
+  getTimeComponents(time) {
+      const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));  
+      const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+      const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+      const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+    
+    return {days, hours, mins, secs};
+    }
+            
+    pad(value) {
+      return String(value).padStart(2, '0');
+    }    
+};
+
+new CountdownTimer({
+  onTick: updateClockface,
+  selector: '#timer-1',
+  targetDate: new Date('Jul 17, 2019'),
+});
+
+function updateClockface({ days, hours, mins, secs }) {
+  //console.log(`${days}:${hours}:${mins}:${secs}`); 
+}
