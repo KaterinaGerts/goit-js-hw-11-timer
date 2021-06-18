253fd46a8495ae479c4f2029a6a69bdb93905b7d
KaterinaGerts <75572090+KaterinaGerts@users.noreply.github.com>
2021-06-18 13:33:33 +0300

    добавила разметку в HTML и применила selector для переиспользования кода

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
  class CountdownTimer {
   constructor ({selector, targetDate}) {
-    this.selector = selector;
+    this.selector = selector; 
+    console.dir(this.selector);   
     this.targetDate = targetDate;
 
     this.start();          
@@ -22,12 +23,14 @@ start() {
       const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
       const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
     
-    console.log({days, hours, mins, secs});
-      document.querySelector('[data-value="days"]').innerHTML = days;
-      document.querySelector('[data-value="hours"]').innerHTML = hours;
-      document.querySelector('[ data-value="mins"]').innerHTML = mins;
-      document.querySelector('[data-value="secs"]').innerHTML = secs;      
-      }
+     console.log({days, hours, mins, secs});
+    
+     document.querySelector(`${this.selector}>.field>[data-value ="days"`).innerHTML = days;
+     document.querySelector(`${this.selector}>.field>[data-value ="hours"`).innerHTML = hours;
+     document.querySelector(`${this.selector}>.field>[data-value ="mins"`).innerHTML = mins;
+     document.querySelector(`${this.selector}>.field>[data-value ="secs"`).innerHTML = secs;
+    
+    }
             
     pad(value) {
       return String(value).padStart(2, '0');
@@ -37,5 +40,10 @@ start() {
 const timer = new CountdownTimer({ 
   selector: '#timer-1',
   targetDate: new Date('Jul 17, 2021'),
+});
+
+const timer2 = new CountdownTimer({ 
+  selector: '#timer-2',
+  targetDate: new Date('Dec 17, 2021'),
 });
 
